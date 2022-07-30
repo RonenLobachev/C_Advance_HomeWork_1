afa5bfa78595eeeca7bcf23be4f901a98fa24885
Ronen <ronen.naale@gmail.com>
2022-07-30 09:09:33 +0300

    Done. Add 2 option for complete this task.

--- Question1/Question1.c
+++ Question1/Question1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define OPTION_1
+
 void generate(char* charSet, int charSetSize, char* phrase, int phraseLen)
 {
 	//halting condition
@@ -12,7 +14,21 @@ void generate(char* charSet, int charSetSize, char* phrase, int phraseLen)
 
 	for (int i = 0; i < charSetSize; i++)
 	{
-
+		//Trick - static variable create and initilized only on first call to function
+		//So all other recursive call will have copy of this variable with last modifed value
+		//In this case we can "fill" phrase array from start to end
+		//Other option is "fill" phrase array is use next line - "phrase[phraseLen - 1] = charSet[i]"
+		//Because on every recursive call will be passed phraseLen - 1 we fill phrase array from end to start
+		//Its more compact but less readable
+		#ifdef OPTION_1
+				static unsigned int u32RecursionDepth = 0;
+				phrase[u32RecursionDepth++] = charSet[i];
+				generate(charSet, charSetSize, phrase, phraseLen - 1);
+				u32RecursionDepth--;
+		#else
+				phrase[phraseLen - 1] = charSet[i];
+				generate(charSet, charSetSize, phrase, phraseLen - 1);
+		#endif
 	}
 }
 
