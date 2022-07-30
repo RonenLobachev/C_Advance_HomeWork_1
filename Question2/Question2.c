ae470702804d709a17271d90a024aa36705dbcc2
Ronen <ronen.naale@gmail.com>
2022-07-30 10:45:44 +0300

    Bug fix. Add function for print array

--- Question2/Question2.c
+++ Question2/Question2.c
@@ -14,41 +14,81 @@ typedef unsigned char bool;
 
 
 //Local Declarations
-bool GetArrayFromUser(unsigned char *out_pu8Array, int* out_i32N)
+bool GetArrayFromUser(unsigned char** out_pu8Array, int* out_i32N);
+void PrintArray(const unsigned char* in_pu8Array, int in_i32N);
+
+/*****************MAIN*****************************/
+void main()
+{
+	int i32row = 0, i32col = 0;
+	unsigned char* pu8Array = NULL;
+	int i32N = 0;
+
+	if (!GetArrayFromUser(&pu8Array, &i32N))
+	{
+		printf("Bad allocation. Programm terminated\n");
+		exit(1);
+	}
+
+	PrintArray(pu8Array, i32N);
+	free(pu8Array);
+	exit(0);
+}
+
+//Service functions
+bool GetArrayFromUser(unsigned char** out_pu8Array, int* out_i32N)
 {
 	bool bRet = TRUE;
 	int i32Tmp = 0;
 	unsigned char u8Tmp = 0;
+	unsigned char* au8Tmp = NULL;
 	do {
 		printf("Enter size of square array:\n");
-		i32Tmp = sscanf("%d", out_i32N);
+		i32Tmp = scanf("%d", out_i32N);
 	} while ((*out_i32N < 0) || (*out_i32N > MAX_ARRAY_COL_ROW));
-	
-	out_pu8Array = (unsigned char*)calloc(pow(*out_i32N,2), sizeof(*out_pu8Array));
-	if (out_pu8Array == NULL)
+
+	*out_pu8Array = (unsigned char*)calloc((int)pow(*out_i32N, 2), (int)sizeof(*out_pu8Array));
+	if (*out_pu8Array == NULL)
 	{
 		bRet = FALSE;
 	}
+	
+	au8Tmp = *out_pu8Array;
 
 	for (int i = 0; (i < *out_i32N) && (bRet != FALSE); i++)
 	{
 		for (int j = 0; j < *out_i32N; j++)
 		{
 			do {
-				printf("Set 0 or 1 to array cell [%d][%d]\n",i,j);
-				i32Tmp = sscanf("%u", &u8Tmp);
+				printf("Set 0 or 1 to array cell [%d][%d]\n", i, j);
+				i32Tmp = scanf("%hhu", &u8Tmp);
 			} while (u8Tmp > 1);
-			out_pu8Array[i * (*out_i32N) + j] = u8Tmp;
+			au8Tmp[i * (*out_i32N) + j] = u8Tmp;
 		}
 	}
+
 	return bRet;
 }
 
-
-void main()
+void PrintArray(const unsigned char* in_pu8Array, int in_i32N)
 {
-	int i32row = 0, i32col = 0;
-	unsigned char* pu8Array = NULL;
-
-
+	if ((in_pu8Array != NULL) && (in_i32N <= 0))
+	{
+		printf("Incoming array have size %dX%d\n", in_i32N, in_i32N);
+	}
+	else
+	{
+		printf("Passed array empty of have not correct lenght\n");
+		return;
+	}
+	
+	for (int i = 0; i < in_i32N; i++)
+	{
+		printf("| ");
+		for (int j = 0; j < in_i32N; j++)
+		{
+			printf("%d | ", in_pu8Array[i * in_i32N + j]);
+		}
+		printf("\n");
+	}
 }
