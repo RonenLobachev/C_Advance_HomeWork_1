41a949f84064b6246e2c2dae000f92dadb23b28a
Ronen <ronen.naale@gmail.com>
2022-07-30 09:07:40 +0300

    Add base code from ex.

--- Question1/Question1.c
+++ Question1/Question1.c
@@ -1,20 +1,37 @@
-// Question1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+void generate(char* charSet, int charSetSize, char* phrase, int phraseLen)
+{
+	//halting condition
+	if (phraseLen == 0)
+	{
+		printf("%s\n", phrase);
+		return;
+	}
+
+	for (int i = 0; i < charSetSize; i++)
+	{
+
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int MAX_PHRASE_LEN = 3;
+	char* phrase = (char*)calloc(MAX_PHRASE_LEN + 1, sizeof(*phrase));
+	if (phrase == NULL)
+		exit(1);
+	char charSet[] = { '1','2','3','4' };
+	int charSetSize = sizeof(charSet) / sizeof(charSet[0]);
+
+	for (int i = 1; i <= MAX_PHRASE_LEN; i++)
+	{
+		printf("all sequences of length %d:\n", i);
+		printf("##########################\n");
+		generate(charSet, charSetSize, phrase, i);
+	}
+
+	free(phrase);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
